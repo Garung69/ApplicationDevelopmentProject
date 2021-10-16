85299fb55c8845491894a7963e52b39ab77574f6
Garung69 <thienlmgch190355@fpt.edu.vn>
2021-10-16 12:06:07 +0700

    Modify Propety DOB

---
+++ ASM/EF/Migrations/202110160328330_ModyfyPropety.cs
@@ -0,0 +1,18 @@
+﻿namespace ASM.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ModyfyPropety : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AspNetUsers", "DoB", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AspNetUsers", "DoB", c => c.DateTime(nullable: false));
+        }
+    }
+}
