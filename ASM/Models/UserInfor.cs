1f95c0575f122801edd2da8595c7842426358a63
Garung69 <thienlmgch190355@fpt.edu.vn>
2021-10-16 08:51:00 +0700

    AddPropety

--- ASM/Models/UserInfor.cs
+++ ASM/Models/UserInfor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,17 +9,36 @@ namespace ASM.Models
 {
     public class UserInfor: IdentityUser
     {
+        public string Role { get; set; }
         public string Department { get; set; }
         public List<CourseEntity> listCourse { get; set; }
         public string Toeic { get; set; }
         public string Name { get; set; }
-        public string DoB { get; set; }
         public string Location { get; set; }
         public string Programming_Language { get; set; }
+        public int Age { get; set; }
+        public string Education { get; set; }
+        public string Type { get; set; }
+        public string WorkingPlace { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DoB { get; set; }
+
+        public string ProgrammingLanguage { get; set; }
+
+        public string Experience { get; set; }
 
         public UserInfor()
         {
             listCourse = new List<CourseEntity>();
+        }
+
+        public string ToSeparatedString(string separator)
+        {
+            return $"{this.Id}{separator}" +
+                $"{this.Name}{separator}"+
+                $"{this.Education}{separator}" +
+                $"{this.Role}";
         }
     }
 }
