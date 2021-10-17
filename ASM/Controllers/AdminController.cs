521a771071a3f268cd76c2d7a011aa496aa7fd1e
Garung69 <thienlmgch190355@fpt.edu.vn>
2021-10-17 07:33:48 +0700

    Create + modify

--- ASM/Controllers/AdminController.cs
+++ ASM/Controllers/AdminController.cs
@@ -13,13 +13,43 @@ namespace ASM.Controllers
 {
     public class AdminController : Controller
     {
+        private void CustomValidationfStaff(UserInfor staff)
+        {
+            if (string.IsNullOrEmpty(staff.Email))
+            {
+                ModelState.AddModelError("Email", "Please input Email");
+            }
+            if (string.IsNullOrEmpty(staff.Name))
+            {
+                ModelState.AddModelError("Email", "Please input Name");
+            }
+            if (!string.IsNullOrEmpty(staff.Email) && (staff.Email.Split('@')[1] != "gmail.com"))
+            {
+                ModelState.AddModelError("Email", "Please a valid Email (abc@gmail.com)");
+            }
+        }
+
+
         [Authorize(Roles = SecurityRoles.Admin)]
         public ActionResult Index()
         {
             using (var ASMCtx = new EF.CMSContext())
             {
                 var Staff = ASMCtx.Users.Where(s => s.Role == "staff").ToList();
-                return View(Staff); 
+                return View(Staff);
+
+/*                using (CMSContext db = new CMSContext())
+                {
+                    var users = (from user in db.Users
+                                 from roles in user.Roles
+                                 join role in db.Roles
+                                 on roles.RoleId equals role.Id
+                                 where role.Name == "admin"
+                                 select new { }
+                                 ).ToList();
+                    return View(users);
+                }*/
+
             }
         }
 
@@ -85,16 +115,12 @@ namespace ASM.Controllers
         [HttpPost]
         public async Task<ActionResult> CreateStaff(UserInfor staff, FormCollection fc)
         {
-            /*            CustomValidationfClass(classRoom);
-                        if (!ModelState.IsValid)
-                        {
-                            //binding gap loi
-                            PrepareViewBag();
-                            TempData["TeacherIds"] = fc["TeacherIds[]"];
-                            return View(classRoom); // return lai Create.cshtml
-                            //di kem voi data ma user da go vao
-                        }
-                        else*/
+            CustomValidationfStaff(staff);
+            if (!ModelState.IsValid)
+            {
+                return View(staff); 
+            }
+            else
             {
                 var context = new CMSContext();
                 var store = new UserStore<UserInfor>(context);
@@ -108,11 +134,10 @@ namespace ASM.Controllers
                     {
                         UserName = staff.Email.Split('@')[0],
                         Email = staff.Email,
-                        Name = staff.Email.Split('@')[0],
                         Role = "staff",
-
+                        PasswordHash = "123qwe123"
                     };
-                    await manager.CreateAsync(user, staff.PasswordHash);
+                    await manager.CreateAsync(user, user.PasswordHash);
                     await CreateRole(staff.Email, "staff");
                 }
                 return RedirectToAction("Index");
@@ -168,23 +193,30 @@ namespace ASM.Controllers
         public async Task<ActionResult> EditStaff(string id, UserInfor staff)
         {
 
-            /*            CustomValidationfStudent(student);
+            CustomValidationfStaff(staff);
 
-                        if (!ModelState.IsValid)
-                        {
-                            PrepareViewBag();
-                            return View(student);
-                        }
-                        else*/
+            if (!ModelState.IsValid)
+            {
+                return View(staff);
+            }
+            else
             {
                 var context = new CMSContext();
                 var store = new UserStore<UserInfor>(context);
                 var manager = new UserManager<UserInfor>(store);
 
-                staff.UserName = staff.Email.Split('@')[0];
-                staff.Name = staff.Email.Split('@')[0];
-                staff.Role = "staff";
-                await manager.UpdateAsync(staff);
+                var user = await manager.FindByEmailAsync(staff.Email);
+
+                if (user != null)
+                {
+
+
+                    user.UserName = staff.Email.Split('@')[0];
+                    user.Email = staff.Email;
+                    user.PasswordHash = "123qwe123";
+                    user.Name = staff.Name;
+                    await manager.UpdateAsync(staff);
+                }             
                 return RedirectToAction("Index");
             }
         }
