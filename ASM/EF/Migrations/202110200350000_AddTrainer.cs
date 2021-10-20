544a815995e55004358903cdbfd824b55787d64a
cuong190622 <87683673+cuong190622@users.noreply.github.com>
2021-10-20 10:51:32 +0700

    Trainer lk

---
+++ ASM/EF/Migrations/202110200350000_AddTrainer.cs
@@ -0,0 +1,16 @@
+﻿namespace ASM.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTrainer : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
